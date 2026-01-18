1a64c2c0fe45194a86aa015577ee7b9ea71cfc8a
Shamo Humbatli <shamohumbatly@gmail.com>
2026-01-18 23:23:54 +0400

    vite conf update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/salt-ai',
+  base: '/salt-ai-web-site',
   plugins: [react(), tailwindcss()],
   
   resolve: {
